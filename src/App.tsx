77d2f51fe590b6abef17320a90d69c6fd7bd98c4
sleduardo20 <sl.eduardo20@gmail.com>
2022-10-10 15:50:36 -0300

    ✨ feat -  integration styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 
-function App() {
+export const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ThemeProvider theme={defaultTheme}>
+     <div className="App">
+     <h1>okok</h1>
     </div>
+    </ThemeProvider>
   );
 }
 
